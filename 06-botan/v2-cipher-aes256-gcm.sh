f8eb82ebf279a9d0ecead0e8f5df9b4c2c5561d9
kevinleake01 <kevinleake01@gmail.com>
2023-08-29 17:11:37 +0000

    Added v2-ciphers to 06-botan

---
+++ 06-botan/v2-cipher-aes256-gcm.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+printf "\n*** IMPORTANT NOTE ***\n\n"
+printf "This uses Botan v2 encryption, and will not run in Botan v3.\n"
+printf "If you are using Botan v3 or later, please use the v3-ciphers.\n\n"
+
+# Example usage:
+# ./v2-cipher-aes256-gcm.sh myfile-0001 "20230830-185214@kevinleake01"
+
+printf "$2" > 00_temp.txt
+
+botan encryption --mode=aes-256-gcm  \
+  --key=000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f  \
+  --iv=0001 --ad=0001 < 00_temp.txt | basenc --base64url > $1.txt
+
+rm 00_temp.txt
+
+qrencode -8 -o $1-qrencode.png -r $1.txt
+zint -b 58 --eci=26 -o $1-zint.png -i $1.txt
+
